db479f9b7f708677db5aa289973b87f3f4c66d9e
Sreenington <sreeaadhi07@gmail.com>
2020-08-15 23:03:45 +0400

    Solved

--- Take a Ten Minute Walk.py
+++ Take a Ten Minute Walk.py
@@ -4,8 +4,7 @@ def is_valid_walk(walk):
     dir_count = (Counter(walk))
     dir_val_x = {'n': 1, 's': -1}
     dir_val_y = {'e': 1, 'w': -1}
-    dx = 0
-    dy = 0
+    dx, dy = 0, 0
     for i in dir_count:
         if i in dir_val_x:
             dx += dir_val_x[i] * dir_count[i]
@@ -17,10 +16,19 @@ def is_valid_walk(walk):
         return print(True)
 
 
+'''
+THIS SOLUTION IS WAY TOO CLEVER(Not mine obv)
+'''
+
+
+def isValidWalk(walk):
+    return len(walk) == 10 and walk.count('n') == walk.count('s') and walk.count('e') == walk.count('w')
+
+
 # Test Cases
 d1 = ['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's']  # True  passes
 d2 = ['w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e']  # False passes
 d3 = ['w']  # False passes
 d4 = ['n', 'n', 'n', 's', 'n', 's', 'n', 's', 'n', 's']  # False passes
 d5 = ['e', 'w', 'n', 'w', 'n', 's', 'n', 's', 'e', 'w']  # False passes
-is_valid_walk(d1)
+is_valid_walk(d5)
