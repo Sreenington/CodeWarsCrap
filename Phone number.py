6e0e1a258135abf21c6a5dcdc9c3d3960029ce97
Sreenington <sreeaadhi07@gmail.com>
2020-08-15 15:55:03 +0400

    Modified

--- Phone number.py
+++ Phone number.py
@@ -1,10 +1,24 @@
-
 import random as r
-# n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+
+def create_phone_number(n):
+    num = ''.join(str(i) for i in n)
+    num = num[:0] + '(' + num[:3] + ') ' + num[3:6] + '-' + num[6:]
+    print(num)
+    return num
+
+
+'''
+Clever Solution(Not mine)
+'''
+
 
 def create_phone_number(n):
     num = "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
     print(num)
     return num
 
-create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+
+create_phone_number(n)
